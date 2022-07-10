96d745a55e03ac6d9895f25c4933134526f9f37f
Berke Ates <beates@student.ethz.ch>
2022-07-10 17:50:54 +0200

    formatting + llc

--- compile.sh
+++ compile.sh
@@ -3,10 +3,6 @@
 # Usage: ./script.sh [cpp file]
 
 # Exactly one global text symbol needed in the source file
-# Make sure the following are installed:
-# clang & clang++
-# Polygeist
-# MLIR
 
 # Settings
 out_dir=./out
@@ -17,15 +13,22 @@ mlir_opt=$(which mlir-opt)               || mlir_opt="NOT FOUND"
 mlir_translate=$(which mlir-translate)   || mlir_translate="NOT FOUND"
 cgeist=$(which cgeist)                   || cgeist="NOT FOUND"
 polygeist_opt=$(which polygeist-opt)     || polygeist_opt="NOT FOUND"
+llc=$(which llc)                         || llc="NOT FOUND"
 
-echo "🔥 clang: $clang"
-echo "   clang++: $clangPP"
-echo "   mlir-opt: $mlir_opt"
-echo "   mlir-translate: $mlir_translate"
-echo "   cgeist: $cgeist"
-echo "   polygeist-opt: $polygeist_opt"
-echo "   output directory: $out_dir"
-echo ""
+fmt_start="🔥 %-18s %s\n"
+fmt_start_nl="\n🔥 %-18s %s\n"
+fmt_list="   %-18s %s\n"
+fmt_err="\n❌ %s\n"
+
+printf "$fmt_start" "clang:" $clang
+printf "$fmt_list" "clang++:" $clangPP
+printf "$fmt_list" "mlir-opt:" $mlir_opt
+printf "$fmt_list" "mlir-translate:" $mlir_translate
+printf "$fmt_list" "cgeist:" $cgeist
+printf "$fmt_list" "polygeist-opt:" $polygeist_opt
+printf "$fmt_list" "llc:" $llc
+printf "$fmt_list" "output directory:" $out_dir
+
 
 set -e # Fail fast
 
@@ -35,8 +38,9 @@ if [ "$clang" == "NOT FOUND" ] || \
    [ "$mlir_opt" == "NOT FOUND" ] || \
    [ "$mlir_translate" == "NOT FOUND" ] || \
    [ "$cgeist" == "NOT FOUND" ] || \
-   [ "$polygeist_opt" == "NOT FOUND" ]; then
-    echo "Please make sure that the necessary tools are installed"
+   [ "$polygeist_opt" == "NOT FOUND" ]|| \
+   [ "$llc" == "NOT FOUND" ]; then
+    printf "$fmt_err" "Please make sure that the necessary tools are installed"
     exit 1;
 fi
 
@@ -50,21 +54,20 @@ rm -rf $out_dir/* # Clear output directory
 src=$1
 src_name=$(basename ${src%.*})
 src_ext=${src##*.}
-echo "🔥 Source: $src_name ($src)"
+printf "$fmt_start_nl" "Source:" "$src_name ($src)"
 
 # Choose compiler based on C or C++
 compiler=$clangPP
 if [ "$src_ext" == "c" ]; then
   compiler=$clang;
 fi
-echo "   Using compiler: $(basename ${compiler%.*})"
+printf "$fmt_list" "Using compiler:" "$(basename ${compiler%.*})"
 
 # Find mangled name
 $compiler -c $src -DUSE_MPI=0 -o $out_dir/$src_name.o
 mangled_name=$(nm $out_dir/$src_name.o | grep -w T | cut -d ' ' -f3)
 rm $out_dir/$src_name.o
-echo "   Mangled function name: $mangled_name"
-echo ""
+printf "$fmt_list\n" "Mangled function name:" "$mangled_name"
 
 # Polygeist c/cpp -> mlir
 $cgeist -resource-dir=$(clang -print-resource-dir) \
@@ -75,25 +78,29 @@ if grep -q -i "polygeist" $out_dir/$src_name.mlir; then
   $polygeist_opt --convert-polygeist-to-llvm \
     $out_dir/$src_name.mlir > $out_dir/$src_name\_polygeist.mlir
   cp $out_dir/$src_name\_polygeist.mlir $out_dir/$src_name.mlir
-  echo "🔥 Lowered Polygeist"
+  printf "$fmt_start" "Lowered Polygeist"
 fi
 
 # Lower affine 
 if grep -q -i "affine" $out_dir/$src_name.mlir; then
   $mlir_opt --lower-affine $out_dir/$src_name.mlir > $out_dir/$src_name\_affine.mlir
   cp $out_dir/$src_name\_affine.mlir $out_dir/$src_name.mlir
-  echo "🔥 Lowered Affine"
+  printf "$fmt_start" "Lowered Affine"
 fi
 
 # Lower to llvm 
 $mlir_opt --lower-host-to-llvm $out_dir/$src_name.mlir > $out_dir/$src_name\_llvm.mlir
 cp $out_dir/$src_name\_llvm.mlir $out_dir/$src_name.mlir
-echo "🔥 Lowered to LLVM"
+printf "$fmt_start" "Lowered to LLVM"
 
 # Translate
 $mlir_translate --mlir-to-llvmir $out_dir/$src_name.mlir > $out_dir/$src_name.ll
-echo "🔥 Translated to LLVMIR"
+printf "$fmt_start" "Translated to LLVMIR"
 
 # Compile
-$clang -c -O3 $out_dir/$src_name.ll > $out_dir/$src_name.o
-echo "🔥 Compiled"
+$llc -O3 $out_dir/$src_name.ll -o $out_dir/$src_name.s
+printf "$fmt_start" "Compiled"
+
+# Assemble
+$clang -c -O3 $out_dir/$src_name.s -o $out_dir/$src_name.o
+printf "$fmt_start" "Assembled"
