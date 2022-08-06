ac6492a749eaa5c08e9676ef05a799a810f26377
Berke Ates <beates@student.ethz.ch>
2022-08-06 19:57:45 +0200

    adjusted example

--- motivating_example/loop.c
+++ motivating_example/loop.c
@@ -2,16 +2,17 @@
 
 int main()
 {
-  int *A = (int *)malloc(2147483645 * sizeof(int));
-  int *B = (int *)malloc(2147483645 * sizeof(int));
+  int *A = (int *)malloc(2147483647 * sizeof(int)); // ~ 8GB of memory
+  int *B = (int *)malloc(21 * sizeof(int));
 
-  for (int i = 0; i < 2147483645; ++i)
+  for (int i = 0; i < 2147483647; ++i)
   {
     A[i] = 5;
 
-    for (int j = 0; j < 2147483645; ++j)
+    for (int j = 0; j < 21; ++j)
     {
       B[j] = A[i];
+      A[j] = A[i];
     }
   }
 
