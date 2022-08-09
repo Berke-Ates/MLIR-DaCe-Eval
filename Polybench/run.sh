aab6afd5a4d6ec83f3c17c742cfd2ed703eca2e7
Berke Ates <beates@student.ethz.ch>
2022-08-09 20:07:55 +0200

    adjusted script

--- Polybench/run.sh
+++ Polybench/run.sh
@@ -6,16 +6,22 @@
 util_folder=./benchmarks/utilities
 driver=./benchmarks/utilities/polybench.c
 flags="-DMEDIUM_DATASET -DPOLYBENCH_DUMP_ARRAYS"
-opt_lvl=O3
+opt_lvl=-O3
 out_dir=./out
+max_time=1m
+repetitions=1
 
+gcc=$(which gcc)                         || gcc="NOT FOUND"
+gpp=$(which g++)                         || gpp="NOT FOUND"
 clang=$(which clang)                     || clang="NOT FOUND"
-clangPP=$(which clang++)                 || clangPP="NOT FOUND"
-mlir_opt=$(which mlir-opt)               || mlir_opt="NOT FOUND"
-cf_opt=$(which cf-opt)                   || cf_opt="NOT FOUND"
-mlir_translate=$(which mlir-translate)   || mlir_translate="NOT FOUND"
+clangpp=$(which clang++)                 || clangpp="NOT FOUND"
 cgeist=$(which cgeist)                   || cgeist="NOT FOUND"
 polygeist_opt=$(which polygeist-opt)     || polygeist_opt="NOT FOUND"
+mlir_opt=$(which mlir-opt)               || mlir_opt="NOT FOUND"
+mlir_translate=$(which mlir-translate)   || mlir_translate="NOT FOUND"
+sdfg_opt=$(which sdfg-opt)               || sdfg_opt="NOT FOUND"
+sdfg_translate=$(which sdfg-translate)   || sdfg_translate="NOT FOUND"
+python=$(which python3)                  || python="NOT FOUND"
 llc=$(which llc)                         || llc="NOT FOUND"
 
 fmt_start="🔥 %-18s %s\n"
@@ -23,27 +29,35 @@ fmt_start_nl="\n🔥 %-18s %s\n"
 fmt_list="   %-18s %s\n"
 fmt_err="\n❌ %s\n"
 
-printf "$fmt_start" "clang:" $clang
-printf "$fmt_list" "clang++:" $clangPP
-printf "$fmt_list" "mlir-opt:" $mlir_opt
-printf "$fmt_list" "cf-opt:" $cf_opt
-printf "$fmt_list" "mlir-translate:" $mlir_translate
+printf "$fmt_start" "gcc:" $gcc
+printf "$fmt_list" "g++:" $gpp
+printf "$fmt_list" "clang:" $clang
+printf "$fmt_list" "clang++:" $clangpp
 printf "$fmt_list" "cgeist:" $cgeist
 printf "$fmt_list" "polygeist-opt:" $polygeist_opt
+printf "$fmt_list" "mlir-opt:" $mlir_opt
+printf "$fmt_list" "mlir-translate:" $mlir_translate
+printf "$fmt_list" "sdfg-opt:" $sdfg_opt
+printf "$fmt_list" "sdfg-translate:" $sdfg_translate
+printf "$fmt_list" "python:" $python
 printf "$fmt_list" "llc:" $llc
 printf "$fmt_list" "output dir:" $out_dir
 printf "$fmt_list" "opt lvl:" $opt_lvl
 
 set -e # Fail fast
-
+   
 # Check if tools exist
-if [ "$clang" == "NOT FOUND" ] || \
-   [ "$clangPP" == "NOT FOUND" ] || \
-   [ "$mlir_opt" == "NOT FOUND" ] || \
-   [ "$cf_opt" == "NOT FOUND" ] || \
-   [ "$mlir_translate" == "NOT FOUND" ] || \
+if [ "$gcc" == "NOT FOUND" ] || \
+   [ "$gpp" == "NOT FOUND" ] || \
+   [ "$clang" == "NOT FOUND" ] || \
+   [ "$clangpp" == "NOT FOUND" ] || \
    [ "$cgeist" == "NOT FOUND" ] || \
    [ "$polygeist_opt" == "NOT FOUND" ]|| \
+   [ "$mlir_opt" == "NOT FOUND" ] || \
+   [ "$mlir_translate" == "NOT FOUND" ] || \
+   [ "$sdfg_opt" == "NOT FOUND" ] || \
+   [ "$sdfg_translate" == "NOT FOUND" ] || \
+   [ "$python" == "NOT FOUND" ] || \
    [ "$llc" == "NOT FOUND" ]; then
     printf "$fmt_err" "Please make sure that the necessary tools are installed"
     exit 1;
@@ -56,79 +70,47 @@ fi
 rm -rf $out_dir/* # Clear output directory
 
 # Get filename
+if [ -z ${1+x} ]; then
+  printf "$fmt_err" "Please provide a C/C++ file"
+  exit 1;
+fi
 src=$1
 src_name=$(basename ${src%.*})
 src_ext=${src##*.}
 src_dir=$(dirname $src)
 printf "$fmt_start_nl" "Source:" "$src_name ($src)"
 
-# Choose compiler based on C or C++
-compiler=$clangPP
-if [ "$src_ext" == "c" ]; then
-  compiler=$clang;
-fi
-printf "$fmt_list" "Using compiler:" "$(basename ${compiler%.*})"
+# Generate executables
+$gcc -I $util_folder $opt_lvl $flags -o $out_dir/$src_name\_gcc.out $src $driver
+printf "$fmt_list" "Generated:" "GCC"
+$gpp -I $util_folder $opt_lvl $flags -o $out_dir/$src_name\_gpp.out $src $driver
+printf "$fmt_list" "Generated:" "G++"
+$clang -I $util_folder $opt_lvl $flags -o $out_dir/$src_name\_clang.out $src $driver
+printf "$fmt_list" "Generated:" "Clang"
+$clangpp -I $util_folder $opt_lvl $flags -o $out_dir/$src_name\_clangpp.out $src $driver &> /dev/null
+printf "$fmt_list" "Generated:" "Clang++"
 
-# Find mangled name
-$compiler -c -I $util_folder $src -o $out_dir/$src_name.o
-mangled_name=$(nm $out_dir/$src_name.o | grep $src_name | cut -d ' ' -f3)
-rm $out_dir/$src_name.o
-printf "$fmt_list\n" "Function name:" "$mangled_name"
+# Generate mlir
 
 # Generate straight translation
 $cgeist -resource-dir=$(clang -print-resource-dir) -I $util_folder \
-  -S --memref-fullrank -$opt_lvl --raise-scf-to-affine $flags \
-  $src > $out_dir/$src_name\_cgeist_nopt.mlir
-printf "$fmt_start" "Generated:" "non-opt-version"
+  -S --memref-fullrank $opt_lvl --raise-scf-to-affine $flags $src | \
+$mlir_opt --affine-loop-invariant-code-motion | $mlir_opt --affine-scalrep | \
+$mlir_opt --lower-affine | $mlir_opt --cse > $out_dir/$src_name\_opt.mlir
+printf "$fmt_start" "Generated:" "Optimized MLIR"
 
 # Polygeist c/cpp -> mlir
-$cgeist -resource-dir=$(clang -print-resource-dir) -I $util_folder \
-  -S --memref-fullrank -$opt_lvl --raise-scf-to-affine $flags \
-  --memref-abi=0 --struct-abi=0 $src > $out_dir/$src_name.mlir
-cp $out_dir/$src_name.mlir $out_dir/$src_name\_cgeist.mlir
-printf "$fmt_list" "Lowered to:" "MLIR"
+$cgeist -resource-dir=$($clang -print-resource-dir) -I $util_folder \
+  -S --memref-fullrank -O0 $flags $src | \
+$mlir_opt --lower-affine --inline > $out_dir/$src_name\_noopt.mlir
+printf "$fmt_list" "Generated:" "Non-optimized MLIR"
 
-# Lower affine 
-if grep -q -i "affine" $out_dir/$src_name.mlir; then
-  $mlir_opt --lower-affine --allow-unregistered-dialect \
-    $out_dir/$src_name.mlir > $out_dir/$src_name\_affine.mlir
-  cp $out_dir/$src_name\_affine.mlir $out_dir/$src_name.mlir
-  printf "$fmt_list" "Lowered:" "Affine"
-fi
-
-# Lower scf 
-if grep -q -i "scf" $out_dir/$src_name.mlir; then
-  $mlir_opt --convert-scf-to-cf --allow-unregistered-dialect \
-     $out_dir/$src_name.mlir > $out_dir/$src_name\_scf.mlir
-  cp $out_dir/$src_name\_scf.mlir $out_dir/$src_name.mlir
-  printf "$fmt_list" "Lowered:" "SCF"
-fi
-
-# Lower cf
-if grep -q -i "cf" $out_dir/$src_name.mlir; then
-  # Workaround for https://github.com/llvm/llvm-project/issues/55301
-  $cf_opt --cf-index-to-int --allow-unregistered-dialect \
-      $out_dir/$src_name.mlir > $out_dir/$src_name\_cf_fix.mlir
-  cp $out_dir/$src_name\_cf_fix.mlir $out_dir/$src_name.mlir
-  printf "$fmt_list" "Applied:" "Workaround"
-
-  $mlir_opt --convert-cf-to-llvm --allow-unregistered-dialect \
-     $out_dir/$src_name.mlir > $out_dir/$src_name\_cf.mlir
-  cp $out_dir/$src_name\_cf.mlir $out_dir/$src_name.mlir
-  printf "$fmt_list" "Lowered:" "CF"
-fi
-
-# Lower Polygeist
-if grep -q -i "polygeist" $out_dir/$src_name.mlir; then
-  $polygeist_opt --convert-polygeist-to-llvm \
-    $out_dir/$src_name.mlir > $out_dir/$src_name\_polygeist.mlir
-  cp $out_dir/$src_name\_polygeist.mlir $out_dir/$src_name.mlir
-  printf "$fmt_list" "Lowered:" "Polygeist"
-fi
+### Lower through MLIR ###
 
 # Lower to llvm 
-$mlir_opt --lower-host-to-llvm $out_dir/$src_name.mlir > $out_dir/$src_name\_llvm.mlir
-cp $out_dir/$src_name\_llvm.mlir $out_dir/$src_name.mlir
+$mlir_opt --convert-scf-to-cf --convert-func-to-llvm --convert-cf-to-llvm \
+  --lower-host-to-llvm --reconcile-unrealized-casts \
+  $out_dir/$src_name\_opt.mlir > $out_dir/$src_name.mlir
 printf "$fmt_list" "Lowered to:" "LLVM"
 
 # Interface renaming function
@@ -155,30 +137,42 @@ $mlir_translate --mlir-to-llvmir $out_dir/$src_name.mlir > $out_dir/$src_name.ll
 printf "$fmt_start_nl" "Translated to:" "LLVMIR"
 
 # Compile
-$llc -$opt_lvl --relocation-model=pic $out_dir/$src_name.ll -o $out_dir/$src_name.s
+$llc $opt_lvl --relocation-model=pic $out_dir/$src_name.ll -o $out_dir/$src_name.s
 printf "$fmt_list" "Compiled using:" "LLC"
 
 # Assemble
-$compiler -c -g -$opt_lvl $out_dir/$src_name.s -o $out_dir/$src_name.o
-printf "$fmt_list" "Assembled using:" "$(basename ${compiler%.*})"
+$clang $opt_lvl $flags $out_dir/$src_name.s $driver -o $out_dir/$src_name\_mlir.out
+printf "$fmt_list" "Assembled using:" "Clang"
 
-# Link
-$compiler $driver $out_dir/$src_name.o $flags \
-  -g -$opt_lvl -o $out_dir/$src_name.out
-printf "$fmt_list" "Linked with:" $driver
+# Compile SDFG
+$sdfg_opt --convert-to-sdfg $out_dir/$src_name\_opt.mlir \
+| $sdfg_translate --mlir-to-sdfg | $python opt.py $out_dir/$src_name\_opt.sdfg
+printf "$fmt_list" "Compiled:" "Optimized SDFG"
 
-# Execute
-printf "$fmt_start_nl" "Executing:" "$out_dir/$src_name.out"
-actual=$(./$out_dir/$src_name.out 2>&1)
-printf "\n%s\n" "$actual"
+$sdfg_opt --convert-to-sdfg $out_dir/$src_name\_noopt.mlir \
+| $sdfg_translate --mlir-to-sdfg | $python opt.py $out_dir/$src_name\_noopt.sdfg
+printf "$fmt_list" "Compiled:" "Non-Optimized SDFG"
 
-# Compare output
-$compiler -I $util_folder $driver $src $flags -$opt_lvl \
-  -o $out_dir/$src_name\_compare.out
-expected=$(./$out_dir/$src_name\_compare.out 2>&1)
+# Run benchmark
+timings=$out_dir/timings.txt
+touch $timings
+expected=$(./$out_dir/$src_name\_clang.out 2>&1)
 
-if [[ "$actual" == "$expected" ]]; then
-  printf "$fmt_start_nl" "Output correct"
-else
-  printf "$fmt_err" "Wrong output"
-fi
+printf "$fmt_list" "Waiting for GC"
+sleep 5
+
+printf "$fmt_start_nl" "Running:" "GCC"
+echo "--- GCC ---" >> $timings
+for i in $(seq 1 $repetitions); do
+  ts=$(date +%s%N)
+  actual=$(./$out_dir/$src_name\_gcc.out 2>&1)
+  echo $((($(date +%s%N) - $ts)/1000000)) >> $timings
+
+  if [[ "$actual" == "$expected" ]]; then
+    printf "$fmt_list" "Output $i:" "Correct"
+  else
+    printf "$fmt_err" "Output $i:" "Incorrect!"
+  fi
+done
+
+
