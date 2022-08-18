284c3abbc5bcf73fb410a46b44bb65f535b8cbe3
Berke Ates <beates@student.ethz.ch>
2022-08-18 13:31:24 +0200

    simplified opt

--- motivating_example/opt.py
+++ motivating_example/opt.py
@@ -4,10 +4,7 @@ import sys
 
 import dace
 import json
-from dace.transformation.pass_pipeline import Pipeline
 from dace.transformation.auto.auto_optimize import auto_optimize
-from dace.transformation.passes.dead_dataflow_elimination import DeadDataflowElimination
-from dace.transformation.passes.constant_propagation import ConstantPropagation
 from dace import SDFG
 
 print("   Using DaCe from: %s" % dace.__file__)
@@ -15,9 +12,7 @@ print("   Using DaCe from: %s" % dace.__file__)
 sdfg = SDFG.from_json(json.load(sys.stdin))
 sdfg.validate()
 
-auto_optimize(sdfg, dace.DeviceType.CPU)
-ConstantPropagation().apply_pass(sdfg, {})
-Pipeline([DeadDataflowElimination()]).apply_pass(sdfg, {})
 sdfg.simplify()
+auto_optimize(sdfg, dace.DeviceType.CPU)
 
 sdfg.save(sys.argv[1])
