f14305763da4ace11e84bc000ba5009c747f86d2
Berke Ates <beates@student.ethz.ch>
2022-08-30 15:42:43 +0200

    added array2stack

--- motivating_example/opt.py
+++ motivating_example/opt.py
@@ -4,7 +4,7 @@ sys.path.insert(0, "/home/xdb/dace")
 
 import dace
 import json
-from dace.transformation.auto.auto_optimize import auto_optimize
+from dace.transformation.auto.auto_optimize import auto_optimize, move_small_arrays_to_stack
 from dace import SDFG
 
 print("   Using DaCe from: %s" % dace.__file__)
@@ -13,6 +13,7 @@ sdfg = SDFG.from_json(json.load(sys.stdin))
 sdfg.validate()
 
 sdfg.simplify()
+move_small_arrays_to_stack(sdfg)
 auto_optimize(sdfg, dace.DeviceType.CPU)
 
 for node, parent in sdfg.all_nodes_recursive():
