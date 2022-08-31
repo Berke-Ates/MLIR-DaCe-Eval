4ab7e9f6e831d2a1c3703308dec0c268972e3a03
Berke Ates <beates@student.ethz.ch>
2022-08-31 15:58:52 +0200

    adjusted opts

--- Polybench/opt_floyd.py
+++ Polybench/opt_floyd.py
@@ -8,6 +8,8 @@ from dace.transformation.auto.auto_optimize import auto_optimize, move_small_arr
 from dace.transformation.passes.scalar_to_symbol import promote_scalars_to_symbols
 from dace.transformation.dataflow.tasklet_fusion import TaskletFusion
 from dace.transformation.interstate import StateFusion
+from dace.transformation.passes.optional_arrays import OptionalArrayInference
+from dace.transformation.passes.constant_propagation import ConstantPropagation
 from dace.transformation.dataflow.trivial_tasklet_elimination import TrivialTaskletElimination
 from dace import SDFG
 
@@ -16,12 +18,17 @@ print("   Using DaCe from: %s" % dace.__file__)
 sdfg = SDFG.from_json(json.load(sys.stdin))
 sdfg.validate()
 
-# promote_scalars_to_symbols(sdfg, integers_only=False)
-# sdfg.simplify()
-# move_small_arrays_to_stack(sdfg)
-# auto_optimize(sdfg, dace.DeviceType.CPU)
+# for i in range(5):
+#     promote_scalars_to_symbols(sdfg)
+#     OptionalArrayInference().apply_pass(sdfg, dict())
+#     ConstantPropagation().apply_pass(sdfg, dict())
+#     sdfg.apply_transformations_repeated([StateFusion])
+
+sdfg.simplify()
+move_small_arrays_to_stack(sdfg)
+auto_optimize(sdfg, dace.DeviceType.CPU)
 # sdfg.apply_transformations_repeated([TrivialTaskletElimination])
-sdfg.apply_transformations_repeated([StateFusion])
+
 # auto_optimize(sdfg, dace.DeviceType.CPU)
 
 for node, parent in sdfg.all_nodes_recursive():
