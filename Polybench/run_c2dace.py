c16b9a93301e08538731fe85c47ff8c5e2e47e3e
Berke Ates <beates@student.ethz.ch>
2022-09-01 13:52:41 +0200

    added c2dace

---
+++ Polybench/run_c2dace.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.path.insert(0, "/home/xdb/dace")
+
+import time
+import dace
+
+sdfg = dace.SDFG.from_file(sys.argv[1])
+obj = sdfg.compile()
+
+for i in range(int(sys.argv[2])):
+    argv_loc = dace.ndarray(shape=(0, ), dtype=dace.dtypes.dtypes.int8)
+    obj(argc_loc=0, _argcount=0, argv_loc=argv_loc)
