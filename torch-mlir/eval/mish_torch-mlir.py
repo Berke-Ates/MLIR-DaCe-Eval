81f3a1d100efdc3bd57a6f91e55a1b11c0d9cc8d
Berke Ates <beates@student.ethz.ch>
2022-10-28 14:30:59 +0200

    added compilation from scratch

--- torch-mlir/eval/mish_torch-mlir.py
+++ torch-mlir/eval/mish_torch-mlir.py
@@ -1,12 +1,32 @@
 import torch
+import torch_mlir
 import sys
 from pathlib import Path
 
 from torch_mlir_e2e_test.mhlo_backends.linalg_on_tensors import LinalgOnTensorsMhloBackend
 
-compiled = Path('mish_llvm.mlir').read_text()
 
+class Mish(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        x = torch.log(1 + torch.exp(x))
+        return x
+
+
+model = Mish()
+model.eval()
+data = torch.rand(8, 32, 224, 224)
 backend = LinalgOnTensorsMhloBackend()
+
+module = torch_mlir.compile(model,
+                            data,
+                            output_type=torch_mlir.OutputType.MHLO)
+
+compiled = backend.compile(module)
+
 jit_module = backend.load(compiled)
 jit_func = jit_module.forward
 
