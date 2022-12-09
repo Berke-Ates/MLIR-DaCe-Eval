b63f0848de8a69a9a35d921e851307751645b56c
Berke Ates <beates@student.ethz.ch>
2022-12-09 09:58:44 +0100

    better runners

--- torch-mlir/eval/mish_pytorch.py
+++ torch-mlir/eval/mish_pytorch.py
@@ -1,5 +1,8 @@
 import torch
 from torch import nn
+import time
+import numpy as np
+import tqdm
 
 
 class Mish(nn.Module):
@@ -14,8 +17,21 @@ class Mish(nn.Module):
 
 model = Mish()
 model.eval()
-data = torch.rand(8, 32, 224, 224)
 
-golden_prediction = model.forward(data)
+for i in range(10):
+    # warmup
+    data = torch.rand(8, 32, 224, 224)
+    model.forward(data)
+
+times = np.zeros(100)
+for i in tqdm.trange(100):
+    data = torch.rand(8, 32, 224, 224)
+    start = time.time()
+    prediction = model.forward(data)
+    times[i] = time.time() - start
+
+print('Median time:', np.median(times))
+
+# golden_prediction = model.forward(data)
 # print("PyTorch prediction")
 # print(golden_prediction)
--- torch-mlir/eval/mish_sdfg.py
+++ torch-mlir/eval/mish_sdfg.py
@@ -1,5 +1,8 @@
 import sys
 import numpy as np
+import time
+import numpy as np
+import tqdm
 
 sys.path.insert(0, "/home/xdb/dace")
 import dace
@@ -8,14 +11,17 @@ sdfg = dace.SDFG.from_file("mish.sdfg")
 obj = sdfg.compile()
 
 for i in range(10):
+    # warmup
+    arr = np.random.rand(8, 32, 224, 224).astype(np.float32)
+    arg1 = np.random.rand(8, 32, 224, 224).astype(np.float32)
+    obj(___constant_8x32x224x224xf32_1=None, _arg0=arr, _arg1=arg1)
 
-    argDict = {}
+times = np.zeros(100)
+for i in tqdm.trange(100):
+    arr = np.random.rand(8, 32, 224, 224).astype(np.float32)
+    arg1 = np.random.rand(8, 32, 224, 224).astype(np.float32)
+    start = time.time()
+    obj(___constant_8x32x224x224xf32_1=None, _arg0=arr, _arg1=arg1)
+    times[i] = time.time() - start
 
-    for argName, argType in sdfg.arglist().items():
-        arr = np.random.rand(8, 32, 224, 224).astype(np.float32)
-        argDict[argName] = arr
-
-    obj(**argDict)
-
-# for argName, arr in argDict.items():
-#     print(arr)
+print('Median time:', np.median(times))
